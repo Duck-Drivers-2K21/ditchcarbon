b71662856baed9a3bbebaebc99bb21544fb0bc85
lc2232 <lc2232@bath.ac.uk>
2023-04-01 21:31:12 +0100

    removed API key

--- src/DitchCarbon/main.py
+++ src/DitchCarbon/main.py
@@ -73,29 +73,3 @@ class DitchCarbonClient:
         locals_ = locals()
         locals_['id'] = None
         return self._call_ditch_carbon(f"activities/{id}", locals_)
-
-
-# carbon_client = DitchCarbonClient(api_key='680de78ec2812d2729138cb7e5b8319e')
-# print("SUPPLIER" + str(carbon_client.get_supplier('Nestle', currency='GBP')))
-#
-# print("EXPENSE" + str(carbon_client.get_expense('Nestle', amount=120, amount_currency='USD')))
-#
-# print("PRODUCT" + str(carbon_client.get_product('Macbook', manufacturer='Apple')))
-#
-# print("FIND SERVER" + str(
-#     carbon_client.find_server(manufacturer_name='Hewlett-Packard', model='ProLiant BL460c G8', ram_bytes=68719476736)))
-#
-# print("RET SERVER" + str(carbon_client.get_server(id='1')))
-#
-# print("GET SERVER EMISSIONS" + str(carbon_client.get_server_emissions(id='1', hours=2)))
-#
-# print("GET CATEGORIES" + str(carbon_client.get_categories()))
-#
-# print("GET ACTIVITY CATEGORIES" + str(carbon_client.get_activity_categories(region='GB')))
-#
-# print("GET ACTIVITIES" + str(carbon_client.get_activities(region='GB')))
-#
-# print("GET ACTIVITY" + str(carbon_client.get_activity(id='act_024b1cd615')))
-#
-# print("GET ACTIVITY ASS" + str(
-#     carbon_client.get_activity_assessment(id='act_dac6b9fe9b', region='GB', declared_unit='tonnes')))
